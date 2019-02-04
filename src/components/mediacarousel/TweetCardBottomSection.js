7e25997b229054e00102e60eff02d08a6118eab8
Alen Thomas <alenthomas9@gmail.com>
2019-02-04 14:03:32 +0530

    Bug fix: Hard code shares & logo for IG posts

--- src/components/mediacarousel/TweetCardBottomSection.js
+++ src/components/mediacarousel/TweetCardBottomSection.js
@@ -4,35 +4,61 @@ import lodash from 'lodash';
 import { formatNumber } from './helpers.js';
 
 export default class TweetCardBottomSection extends Component {
+
+  renderLikes = (likes) => {
+    return likes ? (
+      <div className='likes'>
+        <div className='icon like' />
+        <span>{formatNumber(likes)}</span>
+      </div>
+    ): null;
+  }
+
+  renderShares = (shares) => {
+    return shares ? (
+      <div className="shares">
+        <div className="icon retweet"/>
+        <span>{formatNumber(shares)}</span>
+      </div>
+    ) : null;
+  }
+
+  renderLogo = (logoSvg, logoName) => {
+    return (<img className="twitter-bird" src={logoSvg} alt={logoName}/>)
+  }
+
   render() {
     let embed = this.props.embed;
     let shares = lodash.get(embed, 'metrics.shares');
     let likes = lodash.get(embed, 'metrics.likes');
-    let { type } = this.props.source;
-    let sourceTypeIcon = `/assets/logo/${type}.svg`
-
-    return <div className="tweet-card-bottom">
-      <div className="metrics">
-        { likes ?
-          <div className="likes">
-            <div className="icon like"/>
-            <span>{formatNumber(likes)}</span>
-          </div>
-          : false
-        }
-
-        { shares ?
-          <div className="shares">
-            <div className="icon retweet"/>
-            <span>{formatNumber(shares)}</span>
-          </div>
-          : false
-        }
+    let { name } = this.props.source;
+    if(name === 'Instagram') {
+      return (
+      <div className='tweet-card-bottom'>
+        <div className="metrics">
+        {this.renderLikes(shares)}
+        </div>
+        <div className="twitter-logo">
+          {this.renderLogo('/asset/logo/instagram.svg', 'instagram')}
+        </div>
       </div>
+      )
+    }
 
-      <div className="twitter-logo">
-        <img className="twitter-bird" src={sourceTypeIcon} alt={type}/>
-      </div>
-    </div>
+    if(name === 'Twitter') {
+      return (
+        <div className="tweet-card-bottom">
+          <div className="metrics">
+          {this.renderLikes(likes)}
+          {this.renderShares(shares)}
+          </div>
+          <div className="twitter-logo">
+            {this.renderLogo('/asset/logo/twitter.svg', 'twitter')}
+          </div>
+        </div>
+      )
+    }
+
+    return ''
   }
 }
