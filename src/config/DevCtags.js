860ab80a7e84f62ee5dba94001523c3aa36e893a
Alen Thomas <alenthomas9@gmail.com>
2019-04-12 12:44:48 +0530

    Change tmobile loading video

--- src/config/DevCtags.js
+++ src/config/DevCtags.js
@@ -1274,7 +1274,7 @@ export default {
     "containerCssClassname": "tmobile",
     "background": 'url("/assets/tmobile/bg.jpg") no-repeat',
     "loadingMedia": {
-      "url": () => `https://storage.googleapis.com/animable/tmobile/${lodash.random(1, 2)}.mp4`
+      "url": () => `https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4`
     },
     "layout": {
       "regular": {
