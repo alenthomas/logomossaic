0a97d851319ad13b00f9690ef3671648d701a236
Alen Thomas <alenthomas9@gmail.com>
2019-05-10 17:45:08 +0530

    Add full screen confirmation for marquee

--- src/components/marquee/IndexComponent.js
+++ src/components/marquee/IndexComponent.js
@@ -14,9 +14,55 @@ class IndexComponent extends Component {
     super(props);
 
     this.state = {
-      data: []
+      data: [],
+      fullscreen: false,
+      asked: false,
     };
   }
+
+  handleFullscreenChange = (e) => {
+   let fullscreen = false
+   if (document.fullscreenElement ||
+     document.mozFullScreenElement||
+     document.webkitFullscreenElement ||
+     document.msFullscreenElement ||
+     document.fullscreen ||
+     document.mozFullScreen ||
+     document.webkitIsFullScreene ||
+     document.fullScreenMode ) {
+     fullscreen = true
+   }
+   this.setState ({fullscreen, asked: true})
+ }
+
+ handleToggle = (e) => {
+   const el = document.documentElement
+   if(!this.state.fullscreen) {
+     if (el.requestFullscreen) {
+       el.requestFullscreen()
+     } else if (el.mozRequestFullScreen) {
+       el.mozRequestFullScreen()
+     } else if (el.webkitRequestFullscreen) {
+       el.webkitRequestFullscreen()
+     } else if (el.msRequestFullscreen) {
+       el.msRequestFullscreen()
+     }
+   } else {
+     if (document.exitFullscreen) {
+       document.exitFullscreen()
+     } else if (document.mozCancelFullScreen) {
+       document.mozCancelFullScreen()
+     } else if (document.webkitExitFullscreen) {
+       document.webkitExitFullscreen()
+     } else if (document.msExitFullscreen) {
+       document.msExitFullscreen()
+     }
+   }
+ }
+
+ cancel = (e) => {
+   this.setState({asked: true});
+ }
 
   loadData = (data) => {
     let filtered = data.filter((d) => d.source.name === 'Twitter');
@@ -24,6 +70,11 @@ class IndexComponent extends Component {
   }
 
   componentWillMount() {
+    document.addEventListener('webkitfullscreenchange', this.handleFullscreenChange, false)
+   document.addEventListener('mozfullscreenchange', this.handleFullscreenChange, false)
+   document.addEventListener('msfullscreenchange', this.handleFullscreenChange, false)
+   document.addEventListener('MSFullscreenChange', this.handleFullscreenChange, false) //IE11
+   document.addEventListener('fullscreenchange', this.handleFullscreenChange, false)
     let { marquee: {count, refreshrate} } = this.props.config;
     let params = getQueryString(this.props.location.search);
     watchSocial(this.loadData, handleError, params.ctag, params.filter, count, refreshrate);
@@ -43,12 +94,24 @@ class IndexComponent extends Component {
   }
 
   componentWillUnmount() {
+    document.removeEventListener('webkitfullscreenchange', this.handleFullscreenChange)
+   document.removeEventListener('mozfullscreenchange', this.handleFullscreenChange)
+   document.removeEventListener('msfullscreenchange', this.handleFullscreenChange)
+   document.removeEventListener('MSFullscreenChange', this.handleFullscreenChange)
+   document.removeEventListener('fullscreenchange', this.handleFullscreenChange)
     timeoutCollection.removeAll();
   }
 
   render() {
     let data = this.state.data;
-
+    if(!this.state.fullscreen && !this.state.asked) {
+      return (
+        <div className="ask-fullscreen">
+          <button onClick={this.handleToggle}>Toggle Full Screen</button>
+          <button onClick={this.cancel}>No full screen</button>
+        </div>
+      )
+    }
     return <MarqueeStream data={data} />
   }
 }
