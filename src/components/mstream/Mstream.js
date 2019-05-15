c28c199eef3372ea6d7a52240fff45cb09f534ab
Alen Thomas <alenthomas9@gmail.com>
2019-05-15 18:47:21 +0530

    Clean up remove dead code

--- src/components/mstream/Mstream.js
+++ src/components/mstream/Mstream.js
@@ -5,15 +5,6 @@ import TweetCard from './../mediacarousel/TweetCard';
 import Observer from '@researchgate/react-intersection-observer';
 const uuidv1 = require('uuid/v1');
 
-function displayOption (width) {
-  if(width === 320) {
-    return 'first';
-  }
-  if(width === 640) {
-    return 'second';
-  }
-  return 'first';
-}
 export default class MasonaryStreamV2 extends Component {
   constructor(props) {
     super(props);
@@ -38,8 +29,7 @@ export default class MasonaryStreamV2 extends Component {
   intersected = (e) => {
 
     if(e.isIntersecting) {
-      console.log('>> getting more content: ', e.target.attributes['data-card-id'].value);
-      let v = parseInt(e.target.attributes['data-card-id'].value);
+      let v = parseInt(e.target.attributes['data-card-id'].value, 10);
       this.getMoreContent(v);
     }
   }
@@ -52,28 +42,18 @@ export default class MasonaryStreamV2 extends Component {
     }, () => this.addNewData(v));
   }
 
-  calcTime = () => {
-    let maxHeight = 1080;
-
-  }
-
   addNewData = (v) => {
-    console.log('>> addding new data to : ', v);
-    console.log('>> state: ', this.state.data.length);
-    console.log('>> count: ', this.state.count);
     let min;
     if(this.state.exp === 2) {
       min = Math.max.apply(Math, this.state.count);
-      min=min*2;
+      min *= 2;
     } else {
       min = this.state.count[v];
     }
-    console.log(this.state.data.length - min <= 20);
     if(this.state.data.length - min <= 20) {
       let data = this.props.data;
       let propDataLength = this.props.data.length;
       if(data[data.length-1].id !== this.state.data.slice(0, propDataLength)[propDataLength-1].id) {
-        console.log('here 1');
         this.setState((prevState) => {
           let newData = _.unionBy([...prevState.data, ...data], 'id');
           return ({data: newData})
@@ -81,10 +61,8 @@ export default class MasonaryStreamV2 extends Component {
         return;
       } else {
         this.setState((prevState) => {
-          console.log('here 2');
           let uuid = uuidv1();
           let repeatData = prevState.data.slice(0, 100).map(e => ({...e, id: e.id+uuid}));
-          console.log('data', prevState.data, repeatData);
           return ({data: [...prevState.data, ...repeatData]});
         })
         return;
@@ -93,7 +71,6 @@ export default class MasonaryStreamV2 extends Component {
   }
 
   renderWrapper = (exp) => {
-    // console.log(">> exp: ", exp, this.state);
     const options = {
       onChange: this.intersected,
       root: null,
@@ -104,7 +81,6 @@ export default class MasonaryStreamV2 extends Component {
       let data1 = this.state.data.filter((e, i) => i%2 === 0).map((e, i) => {
         let index = Math.round(this.state.data.length/2) - 4;
         if(i === index) {
-          console.log('>> observer 0: ', i);
           return (
             <Observer {...options} key={e.id}>
               <div data-card-id={0}>
@@ -118,7 +94,6 @@ export default class MasonaryStreamV2 extends Component {
       let data2 = this.state.data.filter((e, i) => i%2 !== 0).map((e, i) => {
         let index = Math.round(this.state.data.length/2) - 4;
         if(i === index) {
-          console.log('>> observer 1: ', i);
           return (
             <Observer {...options} key={e.id}>
               <div data-card-id={1}>
@@ -145,10 +120,9 @@ export default class MasonaryStreamV2 extends Component {
           {this.state.data.map((e, i) => {
             let index = Math.round(this.state.data.length) - 4;
             if(i === index) {
-              console.log(">> observer", i);
               return (
                 <Observer {...options} key={e.id}>
-                  <div data-card-id={0}> {/* TODO remove this wrapper */}
+                  <div data-card-id={0}>
                     <TweetCard data={e} key={e.id} />
                   </div>
                 </Observer>
@@ -162,8 +136,6 @@ export default class MasonaryStreamV2 extends Component {
   }
 
   render() {
-    // console.log(this.props.data, this.state);
-    console.log('>>>>>>>>>>>>>>>>>>', document.getElementById('mstream-wrapper') && document.getElementById('mstream-wrapper').offsetHeight);
     return (
       <div className='mstream'>
         { this.renderWrapper(this.state.exp) }
