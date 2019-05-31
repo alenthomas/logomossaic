5538af57e78f148f953c4d902e78ab7971603539
Alen Thomas <alenthomas9@gmail.com>
2019-05-31 17:39:10 +0530

    Add polyfills for intersection observer

--- src/components/mstream/AnimateScroll.js
+++ src/components/mstream/AnimateScroll.js
@@ -3,6 +3,8 @@ import 'intersection-observer';
 import Observer from '@researchgate/react-intersection-observer';
 import ReactResizeDetector from 'react-resize-detector';
 import './animationscroll.css';
+import _ from 'lodash';
+IntersectionObserver.prototype.POLL_INTERVAL = 500; // Time in milliseconds.
 
 const SPEED = 100.91;
 
@@ -28,55 +30,39 @@ export class AnimateScroll extends Component {
     // return {};
     if(refId === 'one') {
       let time = this.state.heightOne/SPEED;
-      return {animation: `scroll ${time}s linear`}
+      return {animation: `scroll-top ${time}s linear`}
     }
     if(refId === 'two') {
       let time = this.state.heightTwo/SPEED;
-      return {animation: `scroll ${time}s linear`}
+      return {animation: `scroll-top ${time}s linear`}
     }
   }
 
-  manageRef = (refId, ratio) => {
+  manageReff = (refId, ratio, e) => {
     if(refId === 'one') {
-      if(this.state.showOne) {
-        if(ratio > 0 && ratio < 0.2 && this.state.oneInView) {
-          // div entering
-          this.setState({showTwo: true, showOne: false, oneInView: false});
-          return;
-        }
-        // if(ratio > 0 && ratio < 0.2 && this.state.oneInView) {
-        //   // div exiting
-        //   this.setState({showTwo: true, showOne: true, oneInView: false});
-        //   return;
-        // }
-        if(ratio > 0.8 && ratio < 1.1) {
-          this.setState({showTwo: true, showOne: true, oneInView: true});
-          return;
-        }
+      // if(this.state.showOne && ratio > 0.2 && ratio < 0.8) {
+        // this.setState({showOne: true, showTwo: true, oneInView: true});
+        // return;
+      // }
+      if(this.state.showOne && ratio > 0.99) {
+        this.setState({showOne: false, showTwo: true, oneInView: false, twoInView: true});
+        return;
       }
     }
     if(refId === 'two') {
-      if(this.state.showTwo) {
-        // if(ratio > 0 && ratio < 0.2 && !this.state.twoInView) {
-        //   // div entering
-        //   this.setState({showTwo: true, showOne: false, twoInView: true});
-        //   return;
-        // }
-        if(ratio > 0 && ratio < 0.2 && this.state.twoInView) {
-          // div exiting
-          this.setState({showTwo: false, showOne: true, twoInView: false});
-          return;
-        }
-        if(ratio > 0.8 && ratio < 1.1 && !this.state.showOne) {
-          this.setState({showTwo: true, showOne: true, twoInView: true});
-          return;
-        }
+      // if(this.state.showTwo && ratio > 0.2 && ratio < 0.8) {
+        // this.setState({showOne: true, showTwo: true, twoInView: true, oneInView: true});
+        // return;
+      // }
+      if(this.state.showTwo && ratio > 0.99) {
+        this.setState({showOne: true, showTwo: false});
       }
     }
   }
 
+
   handleChange = (e, intersectionObj, refId) => {
-    this.props.children && this.manageRef(refId, e.intersectionRatio)
+    this.props.children && this.manageReff(refId, e.intersectionRatio, e);
   }
 
   render() {
@@ -92,8 +78,8 @@ export class AnimateScroll extends Component {
         >
           <ReactResizeDetector handleWidth={false} handleHeight={true} onResize={(w,h)=>this.onResize(w,h, 'one')} />
           {this.props.children}
-          <Observer onChange={(e, x) => this.handleChange(e, x, 'one')} triggerOnce={true} threshold={[0.1, 1.0]}>
-            <div>{this.props.endData}</div>
+          <Observer onChange={(e, x) => this.handleChange(e, x, 'one')} triggerOnce={false} threshold={[0.1, 1.0]}>
+            <div className={`end ${this.props.children.length > 0 ? '': 'hide'}`}>{this.props.endData}</div>
           </Observer>
         </div>
         <div
@@ -104,8 +90,8 @@ export class AnimateScroll extends Component {
         >
           <ReactResizeDetector handleWidth={false} handleHeight={true} onResize={(w,h)=>this.onResize(w,h, 'two')} />
           {this.props.children}
-          <Observer onChange={(e, x) => this.handleChange(e, x, 'two')} triggerOnce={true} threshold={[0.1, 1.0]}>
-            <div>{this.props.endData}</div>
+          <Observer onChange={(e, x) => this.handleChange(e, x, 'two')} triggerOnce={false} threshold={[0.1, 1.0]}>
+            <div className={`end ${this.props.children.length > 0 ? '': 'hide'}`}>{this.props.endData}</div>
           </Observer>
         </div>
 
