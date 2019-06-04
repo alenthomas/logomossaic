f53232bbc525f4bec7ad4137336249427ff086b1
Alen Thomas <alenthomas9@gmail.com>
2019-06-04 14:40:12 +0530

    Add ar experience styles

--- src/components/arwelcome/IndexComponent.js
+++ src/components/arwelcome/IndexComponent.js
@@ -1,25 +1,28 @@
 import React, {Component} from 'react';
+import './arwelcome.css';
 
 export default class ArWelcome extends Component {
   constructor(props) {
     super(props);
-    this.state = {data: null};
+    this.state = {name: ''};
   }
   componentDidMount() {
-    console.log('hjalskjlksajlk')
     fetch('http://devapi.fankave.com/ids/ar/getName?json=true')
     .then(e => e.json())
-    .then(e => this.setState({data: e}))
-    .catch(e => this.setState({data: {"success":true,"msg":"Name retrieved successfully","data":{"name":"error"}}}))
+    .then(({data}) => this.setState({name: data.name}))
+    .catch(e => {
+      console.error('Error: ', e);
+      this.setState({name: ''})
+    })
   }
   render() {
-    if(this.state.data) {
-      return (
-        <div style={{'color': 'yellow', 'font-size': '1.5em', 'margin-left': '10px'}}>Welcome <span style={{'color': 'yellow', 'font-size': '5em', 'padding-left': '10px'}}>{this.state.data.data.name}</span></div>
-      )
-    }
+    console.log(this.state);
     return(
-      <div>{null}</div>
+      <div className='ar-welcome'>
+        <div className='logo'></div>
+        <div className='welcome'>Welcomes</div>
+        <div className='name'>{this.state.name}</div>
+      </div>
     )
   }
 }
