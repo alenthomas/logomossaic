5dcf80a1ef7bd15a1c78ba8b6a397250d941ba58
sashii98 <sashi@fankave.com>
2019-08-21 16:39:21 -0700

    Add Expo2020

--- src/config/ProdCtags.js
+++ src/config/ProdCtags.js
@@ -1355,4 +1355,62 @@ export default {
       "filterHashtags": ["#howlinhuskies"]
     }
   },
+
+  "expo2020": {
+    "containerCssClassname": "expo2020",
+    "background": "url(/assets/xfl/bg.jpg)",
+    "loadingMedia": {
+      "url": () => `http://storage.googleapis.com/animable/expo2020/loading.mp4`,
+      "urlVertical": () => `http://storage.googleapis.com/animable/clus2019/640/2.mp4`,
+      "urlVertical320": () => `http://storage.googleapis.com/animable/clus2019/320/2.mp4`,
+      "urlVertical640": () => `http://storage.googleapis.com/animable/clus2019/640/2.mp4`
+    },
+    "layout": {
+      "regular": {
+        "header": {
+          "leftLogoUrl": '/assets/expo2020/leftLogo.png',
+          "rightLogoUrl": ''
+        },
+        "footer": {
+          "logoUrl": '/assets/logo/pow-by-fankave-white.png'
+        },
+      },
+      "threecolumn": {
+      }
+    },
+    "conversationvolume": {
+      "title": { 'day': 'Volume This Week', 'hour': "Volume Today" }
+    },
+    "horizontalstream": {
+      "title": "Social Feed"
+    },
+    "leaderboard": {
+      "title": "#XFL2020 Leaderboard"
+    },
+    "masonarystream": {
+      "title": "Social Feed"
+    },
+    "masonarystreamv2": {
+      "hideBgWave": true,
+    },
+    "mediacarousel": {
+      "title": "#Expo2020 Highlights"
+    },
+    "photogrid": {
+      "title": "#Expo2020 Visuals"
+    },
+    "trendingdiscussion": {
+      "title": "#Expo2020 Trending Topics"
+    },
+    "photowall": {
+      "title": "Social Pulse",
+      "tileBgText": "#Expo2020",
+      "hideBgWave": true
+    },
+    "tweetcounts": {
+      "title": "Social Stats",
+      "hideBgWave": true,
+      "filterHashtags": ["#expo2020"]
+    }
+  },
 }
