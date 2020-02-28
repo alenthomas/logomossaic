4488e01812b3b0fccd605c933e003e0e2fefc42b
Alen Thomas <alenthomas9@gmail.com>
2020-02-28 20:04:06 +0530

    Updates

---
+++ precache-manifest.44307e99b26018f7f13c855f75c583a0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2dc6afa7e4cc9578b200ac28bc625a",
+    "url": "/logomossaic/f7ffa099772b4121cfcc.worker.js"
+  },
+  {
+    "revision": "a578fa7f7ad2ea96088d258a1010748d",
+    "url": "/logomossaic/index.html"
+  },
+  {
+    "revision": "e1ecea57a611f4bc0307",
+    "url": "/logomossaic/static/css/2.18f57b05.chunk.css"
+  },
+  {
+    "revision": "1f51bda92d1d1d7c01e3",
+    "url": "/logomossaic/static/css/main.2b215fa7.chunk.css"
+  },
+  {
+    "revision": "e1ecea57a611f4bc0307",
+    "url": "/logomossaic/static/js/2.d5b8f828.chunk.js"
+  },
+  {
+    "revision": "2677e65a05df1b5a2aeaed53d3a19df0",
+    "url": "/logomossaic/static/js/2.d5b8f828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f51bda92d1d1d7c01e3",
+    "url": "/logomossaic/static/js/main.5fe7c564.chunk.js"
+  },
+  {
+    "revision": "bbf046c4940d515529ad",
+    "url": "/logomossaic/static/js/runtime-main.5d50e5d3.js"
+  },
+  {
+    "revision": "033917021be5b09e2e0b00adf0299b21",
+    "url": "/logomossaic/static/media/fontello.03391702.woff2"
+  },
+  {
+    "revision": "1cdbe6ee91240433e315a312f62c25a8",
+    "url": "/logomossaic/static/media/fontello.1cdbe6ee.eot"
+  },
+  {
+    "revision": "7a7ca9e4af193a2471b1343191238d58",
+    "url": "/logomossaic/static/media/fontello.7a7ca9e4.svg"
+  },
+  {
+    "revision": "95d6cd8916171bd33a79ea93cab8bbc9",
+    "url": "/logomossaic/static/media/fontello.95d6cd89.woff"
+  },
+  {
+    "revision": "bf0d5daf71e832ba6089b36052dc3d17",
+    "url": "/logomossaic/static/media/fontello.bf0d5daf.ttf"
+  }
+]);
